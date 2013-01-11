8fd996262cf04658c116c52ab707298ed4732334
MatijaSos <matija.sosic@gmail.com>
2013-01-11 03:12:45 +0100

    Doradio upute

--- src/testing/README.md
+++ src/testing/README.md
@@ -5,6 +5,6 @@ Kad budete raditi svoj klasifikator naslijedite ovu klasu i implementirajte virt
 Onda napraviti svoj main u kojem ga instancirate i pozovite metodu test().
 
 Kako ce se testiranje provoditi odreduje se pomocu konfiguracijske datoteke.
-Unutra je za svaki folder napisano koje se slike koriste za testiranje, a sve ostale dobijete na raspolaganje za ucenje.
+Unutra je za svaki folder napisano koje se slike koriste za testiranje, a sve ostale dobijete na raspolaganje za ucenje. Pogledajte u folderu "examples" kako izgleda primjer konfiguracijske datoteke.
 
 ok doci ce i detaljnije upute kasnije ovo je za pocetak.
--- src/testing/example/SampleClassifier.cpp
+++ src/testing/example/SampleClassifier.cpp
@@ -5,10 +5,29 @@
 
 string SampleClassifier::classify(Mat img)
 {
+    /** Ovo je metoda koja za danu sliku vrati kojoj klasi pripada, tj klasificira je.
+     *
+     *  Ovdje je tipican scenarij: izlucim znacajke iz ulazne slike, klasificiram pomocu 
+     *  struktura podataka koje sam inicijalizirao u learn() metodi.
+     *  
+     */
+
     return "classId";
 }
 
 void SampleClassifier::learn(map< string, vector<Mat> >& learningData)
 {
+    /** Ovo je metoda gdje stvorite strukture specificne za svoj konkretni klasifikator.
+     *  
+     *  Kao ulaz dobijete oznacene slike, mapu formata (imeKlase -> skupSlika).
+     *  Vi sad ovdje dalje izlucujete znacajke iz tih slika, provodite ucenje s kojim stvarate
+     *  strukture koje cete poslije koristiti u klasifikaciji.
+     *
+     *  Uglavnom, kad pozovete test() funkciju (koja je vec implementirana), ona ce pozvati ovu
+     *  metodu koja ce sve srediti tako da dalje moze zvati classify().
+     *  Ova metoda zapravo "priprema tlo" za classify(...) metodu.
+     *
+     */
+
     // Do stuff
 }
