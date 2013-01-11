188aabe41af825a804270baeca7cd32af93b12f0
MatijaSos <matija.sosic@gmail.com>
2013-01-11 03:21:42 +0100

    dodao uputa jos malo

--- src/testing/example/SampleClassifier.hpp
+++ src/testing/example/SampleClassifier.hpp
@@ -9,9 +9,20 @@
 class SampleClassifier : public Classifier
 {
     public: 
+
+        // Virtualna metoda - obavezno implementirati
         virtual string classify(Mat img);
 
+        // Virtualna metoda - obavezno implementirati
         virtual void learn(map< string, vector<Mat> >& learningData);
+
+        
+        // Ovdje ce dalje sad ici metode i podaci specificni za vas klasifikator
+        
+        // npr:
+        // void izluciZnacajke(...);
+        // vector< vector<Double> > koeficijenti;
+
 };
 
 #endif /* end of include guard: SAMPLE_CLASSIFIER */
