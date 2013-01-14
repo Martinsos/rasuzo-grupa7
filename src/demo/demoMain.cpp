7263094e906ddb6a06bee8735ba67ae0ae78a519
igor <igor.jerkovic@fer.hr>
2013-01-14 15:25:09 +0100

    Demo popravljen.

--- src/demo/demoMain.cpp
+++ src/demo/demoMain.cpp
@@ -19,11 +19,11 @@ int main(int argc, char const *argv[])
         
         fprintf(stderr, "You can choose between multiple methods:\n"
                          "1. granlund: Granlund coefficients\n"
-                         "2. hw-knn  : Height-width matching with knn\n"
-                         "3. hw-qe   : HW matching with quadratic error\n"
-                         "4. hw-bayes: HW matching with bayes classificator\n"
-                         "5. hw-swm  : HW matching with SVM classificator\n"
-                         "6. hw-rf   : HW matching with Random Forest\n"
+                         "2. bp-knn  : Body proportions matching with knn\n"
+                         "3. bp-qe   : BP matching with quadratic error\n"
+                         "4. bp-bayes: BP matching with bayes classificator\n"
+                         "5. bp-swm  : BP matching with SVM classificator\n"
+                         "6. bp-rf   : BP matching with Random Forest\n"
                          "HTML report will be generated.\n");
 
         cout << endl;
@@ -37,41 +37,38 @@ int main(int argc, char const *argv[])
     // -------------------- HW matching --------------------- //
     
     if( method.size() >= 2 ) {
-        if (method.substr( 0, 2 ) == "hw")
+        if (method.substr( 0, 2 ) == "bp")
         {
             // Instantiate classifier
             Classifier* hwCl = new HWMatching();
             
             // divide features with silhoutte height
-            string heightRatio = "no";
-            fprintf(stderr, "Divide all features with silhouette height?\n"
-                            "type in: yes/no\n");
-            cin >> heightRatio;
-  
-            bool hr = (heightRatio == "yes");
+            bool hr = false;
 
             // Set params
             pair<string, bool> param;
 
-            if( method == "hw-qe" ) 
+            if( method == "bp-qe" ) 
                    param = make_pair("", hr);
-            else if( method == "hw-knn" ) 
+            else if( method == "bp-knn" ) 
                    param = make_pair("knn", hr);
-            else if( method == "hw-bayes" )
+            else if( method == "bp-bayes" )
                    param = make_pair("bayes", hr);
-            else if( method == "hw-svm" ) 
+            else if( method == "bp-svm" ) 
                    param = make_pair("svm", hr);
-            else if( method == "hw-rf" ) 
+            else if( method == "bp-rf" ) 
                    param = make_pair("random_forest", hr);
             else {
                   cout << "Wrong method typed in" << endl;
                   return 1;
             }
+ 
+            string bpReportName = method + "Report.html";
 
             // Generate report
-            hwCl->test(testConfPath, 3, silPath, "hwReport.html", &param);
+            hwCl->test(testConfPath, 3, silPath, bpReportName, &param);
 
-            cout << "Generated hwReport.html" << endl;
+            cout << "Generated " << bpReportName << endl;
         }
     }
     
