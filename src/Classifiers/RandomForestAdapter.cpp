6d7d833a7bfd1d69472b73b4c927b8ec52c15f9b
Martin Sosic <sosic.martin@gmail.com>
2013-01-14 20:11:03 +0100

    Popravio gresku u RandomForestAdapter.cpp.

--- src/Classifiers/RandomForestAdapter.cpp
+++ src/Classifiers/RandomForestAdapter.cpp
@@ -6,7 +6,7 @@
 void RandomForestAdapter::train(vector< vector<float> >& examples, vector<string>& labels) {
     Mat matExamples, matLabels;
     prepareTrainingData(examples, labels, matExamples, matLabels);
-    int minSamples = std::min(1, (int)round(0.01*examples.size()));
+    int minSamples = std::max(1, (int)round(0.01*examples.size()));
     CvRTParams params = CvRTParams(5, minSamples, 0, false, 10, 0, false, 0, 50, 0.1, CV_TERMCRIT_ITER | CV_TERMCRIT_EPS);
     Mat dummy = Mat();
     rf.train(matExamples, CV_ROW_SAMPLE, matLabels, dummy,  dummy, dummy, dummy, params);
