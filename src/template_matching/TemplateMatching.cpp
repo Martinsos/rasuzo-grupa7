792d17cb1cfe7eaa8fbc35a7b2983819828bb639
MatijaSos <matija.sosic@gmail.com>
2013-01-18 15:50:18 +0100

    Stavio template matching koji radi

--- src/template_matching/TemplateMatching.cpp
+++ src/template_matching/TemplateMatching.cpp
@@ -1,4 +1,3 @@
-
 #include "TemplateMatching.hpp"
 
 using namespace std;
@@ -7,141 +6,134 @@ using namespace cv;
 vector< pair<string, double> > TemplateMatching::classify(Mat img, int resNum)
 {
 
-	vector< pair < string,double> > results;
+    vector< pair < string,double> > results;
 
-	string className;
-	set <string> usedNames;
-	Mat temporaryBW, temporary, testImage;
-	vector<double> values;
-	int closestImage;
-	CvPoint center;
-	vector<pair<string, double> > averages;
-	set<pair<double,string> >::iterator iter;
+    string className;
+    set <string> usedNames;
+    Mat temporaryBW, temporary;
+    vector<double> values;
+    int closestImage;
+    CvPoint center;
+    vector<pair<string, double> > averages;
+    set<pair<double,string> >::iterator iter;
 
-        cvtColor(img, temporaryBW, CV_BGR2GRAY);
+    cvtColor(img, temporaryBW, CV_BGR2GRAY);
 
-        center=findCenter(temporaryBW);
-        testImage=adjustByCenter(temporaryBW, center);
+    center=findCenter(temporaryBW);
 
-        cout << "skup slika: " << templateImages.size() << endl;
-        
-	for(int i=0;i<(int)templateImages.size();i++){
 
-                double match = matching(templateImages[i], testImage);
-                cout << match << endl;
+    Mat testImage=adjustByCenter(temporaryBW, center);
 
-		values.push_back(matching(templateImages[i], testImage));
-	}
-        
-        cout << "velicina rezultata: " << values.size() << endl;
-	
-	double max=0;
-	for(int k=0;k<resNum;++k)
-	{
-		max=0;
-		for(int i=0;i<(int)values.size();++i)   
-		{
-			if(values.at(i)>max)
-			{
-				max=values.at(i);
-				closestImage=i;
-			}
-		}
-                // cout << "ubacujem: " << values[closestImage] << " " << classNames[closestImage] << endl;
-		averages.push_back(make_pair(classNames[closestImage],values[closestImage]));
-		values[closestImage]=-1;
+    
+    for(int i=0;i<(int)templateImages.size();i++){
+        double match = matching(templateImages[i], testImage);
+        values.push_back(matching(templateImages[i], testImage));
+    }
+    
+    double max=0;
+    for(int k=0;k<resNum;++k)
+    {
+            max=0;
+            for(int i=0;i<(int)values.size();++i)
+            {
+                    if(values.at(i)>max)
+                    {
+                            max=values.at(i);
+                            closestImage=i;
+                    }
+            }
+            
+            averages.push_back(make_pair(classNames[closestImage],values[closestImage]));
+            values[closestImage]=-1;
+    }
 
-                cout << "idx maximalnog je: " << closestImage << endl;
-	}
-        
-        /*
-        iter = averages.begin();
-        // cout << "Velicina averagesa: " << averages.size() << endl;
-	while ((int)results.size() < resNum)
-	{
-		className = iter->second;
-		if (usedNames.find(className) == usedNames.end())
-		{
-			results.push_back(make_pair(className, iter->first));
-			usedNames.insert(className);
-		}
-		++iter;
-	}
-        */
-        
-
-        //results.push_back(make_pair("sil0_ivana", 0.97));
-        return averages;
+    return averages;
 }
 
 void TemplateMatching::learn(map < string, vector<Mat> >& learningData, void* param)
 {
-        
-	map < string, vector<Mat> >:: iterator classId;
-	set<pair<double, string> > averages;
-	CvPoint center;
+    
+    map < string, vector<Mat> >:: iterator classId;
+    set<pair<double, string> > averages;
+    CvPoint center;
 
-	for(classId = learningData.begin(); classId != learningData.end();++classId)
-	{
-                cout << "Ucim na: " << classId->first << endl;
-		for(int picture = 0; picture < (int) classId->second.size();++picture )
-		{
-                        classNames.push_back(classId->first);
-			
-                        Mat img = classId->second[picture];
-	                Mat temporaryBW, temporary;
+    for(classId = learningData.begin(); classId != learningData.end();++classId)
+    {
+            
+            for(int picture = 0; picture < (int) classId->second.size();++picture )
+            {
+                    classNames.push_back(classId->first);
 
+                    Mat img = classId->second[picture];
+                    Mat temporaryBW, temporary;
+                                            
+                    cvtColor(img, temporaryBW, CV_BGR2GRAY);
+                    center=findCenter(temporaryBW);
+                    temporary=adjustByCenter(temporaryBW,center);
 
-
-                        // img = img.t();
-			cvtColor(img, temporaryBW, CV_BGR2GRAY);
-
-			center=findCenter(temporaryBW);
-			temporary=adjustByCenter(temporaryBW,center);
-			
-			templateImages.push_back(temporary);
-		}
-	}
-        
-        // cout << "jesu li isti: " << templateImages[0] == templateImages[30] << endl;
-        cout << "ucenje proslo!" << endl;
+                    templateImages.push_back(temporary);
+            }
+    }
+     
 }
 
 double TemplateMatching::matching(Mat image, Mat testImage)
 {
-	int rows=image.rows, cols=image.cols;
-	double countMatch=0;
-	for(int i=0;i<rows;i++)
-	{
-		for(int j=0;j<cols;j++)
-		  {
-			 if(image.at<uchar>(i,j)==testImage.at<uchar>(i,j))
-				countMatch++;
-		  }
-	}
-   return countMatch/(rows*cols);
+    int rows=image.rows, cols=image.cols;
+    double countMatch=0;
+    for(int i=0;i<rows;i++)
+    {
+            for(int j=0;j<cols;j++)
+            {
+                    if(image.at<uchar>(i,j)==testImage.at<uchar>(i,j))
+                            countMatch++;
+            }
+    }
+return countMatch/(rows*cols);
 }
 
 CvPoint TemplateMatching::findCenter(Mat image)
 {
-	int sum_x=0,sum_y=0,n=0;
-	int x,y;
-	for (int i=0;i<image.rows;++i){
-		for(int j=0;j<image.cols;++j){
-			if(image.at<uchar>(i,j)!=0){
-				sum_x+=i;
-				sum_y+=j;
-				n++;
-			}
-		}
-	}
-	x=sum_x/n; y=sum_y/n;
+    int sum_x=0,sum_y=0,n=0;
+    int x,y;
+    for (int i=0;i<image.rows;++i){
+            for(int j=0;j<image.cols;++j){
+                    if(image.at<uchar>(i,j)!=0){
+                            sum_x+=i;
+                            sum_y+=j;
+                            n++;
+                    }
+            }
+    }
+    x=sum_x/n; y=sum_y/n;
 
-	return cvPoint(x,y);
+    return cvPoint(x,y);
 }
 
 Mat TemplateMatching::adjustByCenter(Mat image, CvPoint center)
 {
-        // return image(Range(center.x-485,center.x+495),Range(center.y-195,center.y+195));
-        return image;
+    Mat returnImage;    
+
+    int range_x_up=center.x-485;
+
+    int range_x_down=center.x+495;
+    int range_y_up=center.y-195;
+    int range_y_down=center.y+195;
+    if(range_x_up<0){
+            range_x_down+=abs(range_x_up);
+            range_x_up=0;
+    }else if(range_x_down>image.rows){
+            range_x_up-=abs(image.rows - range_x_down);
+            range_x_down=image.rows-1;
+    }
+    if(range_y_up<0){
+            range_y_down+=abs(range_y_up);
+            range_y_up=0;
+    }else if(range_y_down>image.cols){
+            range_y_up-=abs(image.cols - range_y_down);
+            range_y_down=image.cols-1;
+    }
+    returnImage = image(Range(range_x_up,range_x_down),Range(range_y_up,range_y_down));
+
+    return returnImage;
 }
